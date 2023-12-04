92ffe07a3bf4f96f75529bcedd7eb7f8ad2cbe68
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-12-04 15:56:12 -0300

    4/12/2023-15:56

---
+++ src/components/GiftList.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const GiftList = () => {
+  const [gifts, setGifts] = useState([
+    { id: 1, name: 'Gift' },
+    { id: 2, name: 'Gift2' },
+    { id: 3, name: 'Gift3' }
+  ]);
+
+  return (
+    <div>
+      <h2>Gift List</h2>
+      <ul>
+        {gifts.map(gift => (
+          <li key={gift.id}>{gift.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
+export default GiftList;
+
+
